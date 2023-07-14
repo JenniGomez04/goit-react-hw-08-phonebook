aaa8c865ffc14603b76a3f9f29cf34c2321d05c7
Jennifer Gómez <jengr42@gmail.com>
2023-07-13 20:33:17 -0500

    Homework JWT

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { store, persistor } from './redux/store';
 // Crear el punto de montaje para la aplicación en el elemento con id 'root'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>  {/*Modo estricto de React para detectar posibles problemas en el código */}
-    <BrowserRouter basename='goit-react-hw-08-phonebook'> {/* Configuración del enrutador de navegación para la aplicación */}
+    <BrowserRouter basename="/goit-react-hw-08-phonebook"> {/* Configuración del enrutador de navegación para la aplicación */}
       <Provider store={store}> {/* Proveedor de Redux para proporcionar el estado global a los componentes */}
         <PersistGate loading={null} persistor={persistor}> {/* Componente de puerta de enlace de persistencia para cargar y almacenar el estado persistente */}
           <App /> {/* Componente raíz de la aplicación */}
